aa16c78440ad45ff163555befedfa500c8d3467b
Denes <jose.araujo@zup.com.br>
2022-04-12 14:34:18 -0300

    estrutura basica

---
+++ src/test/java/br/com/zup/edu/album/AlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
